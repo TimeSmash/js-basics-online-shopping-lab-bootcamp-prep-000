aab102942aa17c1945fa3ff29777c29581b9f81e
mcumm64 <mcumm64@gmail.com>
2018-11-27 23:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ // item is a string, name of whatever item
+ var itemTitle = `${item}`;
+ 
+ var itemCost = Math.floor(Math.random() *Math.ceil(1) * Math.floor(100));
+ var cartAddObject = cart.push({itemName: itemTitle, itemPrice: itemCost});
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+
+  if (cart.length) {
+    var cartContents = "In your cart, you have "
+    
+    } else {
+      var emptyCartStatement = "Your shopping cart is empty"
+    }
 }
 
 function total() {
