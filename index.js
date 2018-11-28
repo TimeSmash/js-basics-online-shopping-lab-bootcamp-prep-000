b7958894215748c7faa132a1dc1551ed443120f3
mcumm64 <mcumm64@gmail.com>
2018-11-28 03:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ // item is a string, name of whatever item
+ var itemTitle = `${itemName}`;
+ 
+ var itemCost = Math.floor(Math.random() *Math.ceil(1) * Math.floor(100));
+ var cartAddObject = cart.push(itemObject);
+ var itemObject = {itemName: itemTitle, itemPrice: itemCost};
+ return `${itemName} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+
+  if (cart.length) {
+    var cartContents = `In your cart, you have ${cart[itemTitle]}`
+    for (var i =0; i < cart.length; i++) {
+  cart[i] = "" + cart[i]
+    }
+  } else {
+    var emptyCartStatement = "Your shopping cart is empty"
+    }
 }
 
 function total() {
@@ -22,9 +37,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    
+  }
+  else{
+  }
 }
