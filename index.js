1434c933ba1839c73ea276c6c8ebee4dc8dc8017
mcumm64 <mcumm64@gmail.com>
2018-11-29 01:51:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,36 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ // item is a string, name of whatever item
+ 
+ var itemPrice = Math.floor(Math.random() *Math.ceil(1) * Math.floor(100));
+ var itemObject = {itemName: itemName, itemPrice: itemPrice};
+ var cartAddObject = cart.push(itemObject);
+ cartAddObject;
+ return `${itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  //needs to loop over every item in cart, returning contents like "itemName at itemPrice"
+//how to access key in object?
+  if (cart.length) {
+    var cartContents = `In your cart, you have `;
+      if (cart.length === 1) {
+      return cartContents + `${cart.itemName} at ${cart.itemPrice}`;
+      } 
+        else if (cart.length === 2) {
+        var filler = "bla"
+        } else {
+          //code for more than 2 items goes here
+        }
+  }
+  else {
+    for (var i = 0; i < cart.length; i++) {
+  cart[i] = "" + cart[i]
+    }
+    var emptyCartStatement = "Your shopping cart is empty"
+    }
 }
 
 function total() {
@@ -22,9 +46,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    
+  }
+  else{
+  }
 }
