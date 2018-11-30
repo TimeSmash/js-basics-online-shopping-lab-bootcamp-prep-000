b758f7d7c7205a0792f776319734a472c20658b3
mcumm64 <mcumm64@gmail.com>
2018-11-30 02:52:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,22 +9,75 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ // item is a string, name of whatever item
+ 
+ var itemPrice = Math.floor(Math.random() *Math.ceil(1) * Math.floor(100));
+ var itemObject = {itemName: itemName, itemPrice: itemPrice};
+ var cartAddObject = cart.push(itemObject);
+ cartAddObject;
+ return `${itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  //needs to loop over every item in cart, returning contents like "itemName at itemPrice"
+//how to access key in object?
+  if (cart.length) {
+    var cartContents = `In your cart, you have `;
+      if (cart.length === 1) {
+      return cartContents + `${cart[0].itemName} at $${cart[0].itemPrice}.`;
+      } 
+        else if (cart.length === 2) {
+        return cartContents + `${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+        } else if (cart.length >= 3){
+          var cartItems = "";
+          //[1,2,3,4,5] 5 elements long, 5 = index 4
+          for (var i = 0; i < cart.length - 1; i ++) {
+            cartItems = cartItems + `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+          }
+          return cartContents + cartItems + `and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`;
+        }
+  }
+  else {
+    var emptyCartStatement = "Your shopping cart is empty.";
+    return emptyCartStatement;}
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0;
+  if (cart.length) {
+  for (var i = 0; i < cart.length; i++){
+  totalPrice = totalPrice + cart[i].itemPrice;
+  } 
+  return totalPrice;
+  } else {
+    return 0;
+    } 
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++){
+ //check if argument as a string equals itemName value of each element
+  if(cart[i].itemName === `${item}`){
+    cart.splice(i,1);
+    return cart;
+  } else {
+    var notInCart = "That item is not in your cart.";
+    return notInCart;
+  }
 }
+}
+    //item = item to be removed from cart
+  //if itemName is in the cart, take it out, and return updated cart. 
+  // if (cart[itemName])
+  // Use splice to remove whatever you don't want, and then replace it with no arguments to fill the gap
+  // ELSE (itemName not in cart) var notInCart = "That item is not in your cart."
+
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    
+  }
+  else{
+  }
 }
