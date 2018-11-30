14fa91ef019af00d32a4bc40efac967537b39e05
mcumm64 <mcumm64@gmail.com>
2018-11-30 01:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,22 +9,57 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ // item is a string, name of whatever item
+ 
+ var itemPrice = Math.floor(Math.random() *Math.ceil(1) * Math.floor(100));
+ var itemObject = {itemName: itemName, itemPrice: itemPrice};
+ var cartAddObject = cart.push(itemObject);
+ cartAddObject;
+ return `${itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  //needs to loop over every item in cart, returning contents like "itemName at itemPrice"
+//how to access key in object?
+  if (cart.length) {
+    var cartContents = `In your cart, you have `;
+      if (cart.length === 1) {
+      return cartContents + `${cart[0].itemName} at ${cart.itemPrice}`;
+      } 
+        else if (cart.length === 2) {
+        var filler = "bla"
+        } else {
+          //code for more than 2 items goes here
+        }
+  }
+  else {
+    for (var i = 0; i < cart.length; i++) {
+  cart[i] = "" + cart[i]
+    }
+    var emptyCartStatement = "Your shopping cart is empty"
+    }
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0;
+  if (cart.length) {
+  for (var i = 0; i < cart.length; i++){
+  totalPrice = totalPrice + cart[i].itemPrice;
+  } 
+  return totalPrice;
+  } else {
+    return 0;
+    } 
 }
-
 function removeFromCart(item) {
-  // write your code here
+  cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    
+  }
+  else{
+  }
 }
